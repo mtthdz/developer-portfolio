7757ef1ad90aee9757d32b3b51844c7e62b57641
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-08-17 16:50:56 -0400

    added swup library via npm

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,32 @@
-const main = {
+// const main = {
 
-}
+// }
 
-main.hover = () => {
-  $(".project__title").hover(
-    function () {
-      $(this).css("color", "#b3b3d3")
-      $(this).css("letter-spacing", "-2px")
-      $(this).next().css("opacity", "1.0")
-      $(this).next().css("right", "80px")
-    },
-    function () {
-      $(this).css("color", "#ffffff")
-      $(this).css("letter-spacing", "normal")
-      $(this).next().css("opacity", "0")
-      $(this).next().css("right", "0px")
-    }
-  )
-}
+// main.hover = () => {
+//   $(".project__title").hover(
+//     function () {
+//       $(this).css("color", "#b3b3d3")
+//       $(this).css("letter-spacing", "-2px")
+//       $(this).next().css("opacity", "1.0")
+//       $(this).next().css("right", "80px")
+//     },
+//     function () {
+//       $(this).css("color", "#ffffff")
+//       $(this).css("letter-spacing", "normal")
+//       $(this).next().css("opacity", "0")
+//       $(this).next().css("right", "0px")
+//     }
+//   )
+// }
 
-main.init = function () {
-  this.hover();
-}
+// main.init = function () {
+//   this.hover();
+// }
 
-$(function () {
-  main.init();
-})
+// $(function () {
+//   main.init();
+// })
+
+const swup = new Swup({
+  plugins: [new SwupBodyClassPlugin()]
+});
